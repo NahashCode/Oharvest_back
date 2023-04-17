e805fe6c2c451706ea0a12bd6bbe9511cc6151e9
Guillaume Felicite <felicite.guillaume@gmail.com>
2023-04-17 12:04:04 +0200

    adding delete functions and alter table for constraint foreign key (#17)

--- migrations/db_version.sh
+++ migrations/db_version.sh
@@ -5,3 +5,4 @@ sqitch add 4.crud_plot -n "crud function for the plot table"
 sqitch add 5.crud_product -n "crud function for the product table"
 sqitch add 6.crud_variety -n "crud function for variety table"sqitch 
 sqitch add 7.product_in_plot -n "create link table between product and plot with view and functions"
+sqitch add 8.delete_functions -n "adding functions delete to all tables"
