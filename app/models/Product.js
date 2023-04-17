33b55019242d97e530b0b329a3bf5ac95b230c8d
Kévin HESSE <108680561+Kevin-HESSE@users.noreply.github.com>
2023-04-17 11:06:34 +0200

    Feature/setting product available route (#15)
    * setting up api route with swagger documentation

--- app/models/Product.js
+++ app/models/Product.js
@@ -3,6 +3,18 @@ import pool from '../services/pgClient.js';
 
 export class Product extends CoreDataMapper {
     tableName = 'product';
+
+    /**
+     * Query the database to find all products which are available.
+     * @returns {Object[]} An array of product
+     */
+    async findAllProductAvailable(){
+        const query = `SELECT * FROM "${this.tableName}_select" "ps" WHERE "ps"."isAvailable" = true`;
+
+        const result = await this.client.query(query);
+
+        return result.rows;
+    }
 }
 
 export const productDataMapper = new Product(pool);
