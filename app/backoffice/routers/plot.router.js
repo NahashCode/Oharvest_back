08e9c86223a2cb9b2a84881f4f88cb6c18e0a633
Kévin HESSE <108680561+Kevin-HESSE@users.noreply.github.com>
2023-04-20 10:36:18 +0200

    Feature/backoffice crud (#24)
    * add crud for category, plot, product, variety in the backoffice
    
    * check crud action for product, variety, plot and category from back-office. Add the detail page for each one

--- app/backoffice/routers/plot.router.js
+++ app/backoffice/routers/plot.router.js
@@ -1,11 +1,28 @@
 import { Router} from 'express';
 import { plotController } from '../controllers/plotController.js';
+import { plotMiddleware } from '../../middlewares/plotMiddleware.js';
+import { plotValidate } from '../../services/validator/plot/validate.js';
 
 const plotRouter = Router();
 
 /**
  * Route : /admin/plots
  */
-plotRouter.get('/', plotController.allPlot);
+plotRouter.get('/', plotController.listPage);
+plotRouter.get('/create', plotController.createPage);
+plotRouter.post('/create', plotValidate.validateBody, plotController.createAction);
+
+plotRouter.param('id', plotMiddleware.loadPlot);
+plotRouter.get('/:id(\\d+)/detail', plotController.detailPage);
+/**
+ * Route : /admin/categories/:id/edit
+ */
+plotRouter.get('/:id(\\d+)/edit', plotController.editPage);
+plotRouter.post('/:id(\\d+)/edit', plotValidate.validateBody, plotController.editAction);
+
+/**
+ * Route : /admin/categories/:id/delete
+ */
+plotRouter.get('/:id(\\d+)/delete', plotController.deleteAction);
 
 export { plotRouter };
--- app/backoffice/routers/product.router.js
+++ app/backoffice/routers/product.router.js
@@ -1,11 +1,29 @@
 import { Router} from 'express';
 import { productController } from '../controllers/productController.js';
+import { productValidate } from '../../services/validator/product/validate.js';
+import { productMiddleware } from '../../middlewares/productMiddleware.js';
+import { categoryMiddleware } from '../../middlewares/categoryMiddleware.js';
 
 const productRouter = Router();
 
 /**
  * Route : /admin/products
  */
-productRouter.get('/', productController.allProduct);
+productRouter.get('/', productController.listPage);
+productRouter.get('/create', categoryMiddleware.loadCategories,  productController.createPage);
+productRouter.post('/create', productValidate.validateBody, productController.createAction);
+
+productRouter.param('id', productMiddleware.loadProduct);
+productRouter.get('/:id(\\d+)/detail', categoryMiddleware.loadCategories, productController.detailPage);
+/**
+ * Route : /admin/products/:id/edit
+ */
+productRouter.get('/:id(\\d+)/edit', categoryMiddleware.loadCategories, productController.editPage);
+productRouter.post('/:id(\\d+)/edit', productValidate.validateBody, productController.editAction);
+
+/**
+ * Route : /admin/products/:id/delete
+ */
+productRouter.get('/:id(\\d+)/delete', productController.deleteAction);
 
 export { productRouter };
